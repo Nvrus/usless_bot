a8a67d64a50cf07198bbeb650e6424df465b881c
nvrus <nvrus.arsenal@mail.ru>
2023-02-14 05:34:52 +0700

    commit. gift for valentine day

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Install requirements.txt
 Do a telebot for job and fun
 1. create a file settings.py
-2. create a TOKEN = "<insert your bot token>"
-3. create a GROUP_CHAT_ID = "-<insert your group id>"
+2. create a TOKEN = "***insert your bot token***"
+3. create a GROUP_CHAT_ID = "-***insert your group id***"
 4. for more info to create and take a GROUP_CHAT_ID use:
 https://lumpics.ru/how-find-out-chat-id-in-telegram/
 In this ru side you can give more info. USE IT
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
 from settings import TOKEN as bot_token
 from settings import GROUP_CHAT_ID as chat_id
+import sys
+import random
 
 
 bot = telebot.TeleBot(bot_token)
@@ -9,6 +11,12 @@ bot = telebot.TeleBot(bot_token)
 def getdays(message):
     with open('database.txt') as db:
         bot.send_message(message.chat.id, str(int(db.read()) +1))
+
+@bot.message_handler(commands=['gift'])
+def getdays(message):
+    with open('for_you.jpg') as db:
+        bot.send_photo(message.chat.id, photo=open('for_you.jpg', 'rb'))
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
