73c08d599ca81818011bbaec7cabc35eb5b4d636
nvrus <nvrus.arsenal@mail.ru>
2023-02-14 04:55:34 +0700

    commit. prepie to add new functional

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import requests
+import telebot
 from settings import TOKEN as bot_token
 from settings import GROUP_CHAT_ID as chat_id
 
 
-for i in range(5):
-    requests.get('https://api.telegram.org/bot%s/sendMessage' % bot_token,
-             params={
-                 "chat_id": chat_id,
-                 'text': 'My say hello very well'
-             })
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['getdays'])
+def getdays(message):
+    with open('database.txt') as db:
+        bot.send_message(message.chat.id, str(int(db.read()) +1))
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
